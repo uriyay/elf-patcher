a79e91acabab12ebe8d14b624db25c3eab743a51
uriyay <uriyay@gmail.com>
2016-06-15 22:40:43 +0300

    Added Numbers to repo

---
+++ Numbers/converter.py
@@ -0,0 +1,28 @@
+def to_unsigned(signed, bits_number):
+    temp = signed + 2**bits_number
+    return temp & (2**bits_number - 1)
+
+def to_signed(unsigned, bits_number):
+    if unsigned & ((1 << bits_number) - 1):
+        #its negative number
+        return unsigned - (2**bits_number)
+    return unsigned
+
+def to_unsigned_int(signed_int):
+    return to_unsigned(signed_int, 32)
+
+def to_unsigned_short(signed_short):
+    return to_unsigned(signed_short, 16)
+
+def to_unsigned_byte(signed_byte):
+    return to_unsigned(signed_byte, 8)
+
+def to_signed_int(unsigned_int):
+    return to_signed(unsigned_int, 32)
+
+def to_signed_short(unsigned_short):
+    return to_signed(unsigned_short, 16)
+
+def to_signed_byte(unsigned_byte):
+    return to_signed(unsigned_byte, 8)
+
---
+++ Numbers/operators.py
@@ -0,0 +1,7 @@
+import math
+
+def root(n_root, x):
+    '''
+    Returns the nth root of x
+    '''
+    return math.pow(x, (float(1)/n_root))
